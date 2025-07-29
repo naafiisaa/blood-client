653dc0281d4c816995e01211ad124fd64ccc68da
Nafis <nafisac45@gmail.com>
2025-07-29 19:09:08 +0600

    AuthProvider Done

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
-// import { app } from "../firebase/firebase.config";
+
 
 import app from "../firebase/firebase.config";
 import useAxiosPublic from "../Hooks/useAxiosPublic";
